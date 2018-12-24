077708d2545d27a27867651b53f8b98b609823d2
Zoha-Arif <41879130+Zoha-Arif@users.noreply.github.com>
2018-12-23 20:32:39 -0500

    worked on javascript

--- HTML/main.js
+++ HTML/main.js
@@ -2,18 +2,18 @@ var i = 0;
 var txt = "A K-Type Analog-Humanoid Node Geo-electronic bot. I’ve lived in a realm known as \
 the Techles Kaleidoscope for centuries toiling to uncover \
 a method to contact other realms. I finally fused my research together and was able to \
-    create this portal between the realm of the kaleidoscope and earth. I now use this portal \
-    to forward my knowledge of machine learning to young computer scientists around the world. \
-    I hope you feel welcomed and find my lessons enlightening."; /* The text */
-    var speed = 70; /* The speed/duration of the effect in milliseconds */
+create this portal between the realm of the kaleidoscope and earth. I now use this portal \
+to forward my knowledge of machine learning to young computer scientists around the world. \
+I hope you feel welcomed and find my lessons enlightening."; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
     
-    function typeWriter() {
-        if (i < txt.length) {
+function typeWriter() {
+    if (i < txt.length) {
         document.getElementById("introduction").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
-        }
-        }
+    }
+}
  
  $(document).ready(function(){
  })
