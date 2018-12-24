88121b0c63734b01e5deff4415d54643cbf98cc1
Zoha-Arif <41879130+Zoha-Arif@users.noreply.github.com>
2018-12-23 21:34:48 -0500

    fixed javascript

--- HTML/main.js
+++ HTML/main.js
@@ -7,7 +7,7 @@ a method to contact other realms. I finally fused my research together and was a
 create this portal between the realm of the kaleidoscope and earth. I now use this portal \
 to forward my knowledge of machine learning to young computer scientists around the world. \
 I hope you feel welcomed and find my lessons enlightening."; /* The text */
-var speed = 70; /* The speed/duration of the effect in milliseconds */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
     
 function typeWriter() {
     if (i < txt.length) {
@@ -15,7 +15,26 @@ function typeWriter() {
         i++;
         setTimeout(typeWriter, speed);
     }
+    var h = i + 1; 
+    if (h == txt.length){
+        var x = document.getElementById("cursor");
+        x.style.display = "block";
+    }
 }
 
 typeWriter.call(); 
+
 })
+
+var cursor = true;
+var speed = 220;
+
+setInterval(() => {
+   if(cursor) {
+     document.getElementById('cursor').style.opacity = 0;
+     cursor = false;
+   }else {
+     document.getElementById('cursor').style.opacity = 1;
+     cursor = true;
+   }
+}, speed);
