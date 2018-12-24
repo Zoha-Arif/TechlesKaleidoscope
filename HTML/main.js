6a91ef3f14e56fc85c89f382f459bdf8f2b13f96
Zoha-Arif <41879130+Zoha-Arif@users.noreply.github.com>
2018-12-24 11:29:55 -0500

    fianlized change to mango

--- HTML/main.js
+++ HTML/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 var i = 0;
-var txt = "A K-Type Analog-Humanoid Node Geo-electronic bot. I’ve lived in a realm known as \
+var txt = "A M-Type Analog-Humanoid Node Geo-electronic Orbital bot. I’ve lived in a realm known as \
 the Techles Kaleidoscope for centuries toiling to uncover \
 a method to contact other realms. I finally fused my research together and was able to \
 create this portal between the realm of the kaleidoscope and earth. I now use this portal \
