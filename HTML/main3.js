acdf1aa10af961766a430f7e144afbf815ecac51
Zoha-Arif <41879130+Zoha-Arif@users.noreply.github.com>
2022-01-01 21:09:52 -0500

    everything

---
+++ HTML/main3.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      color: '#fff'
+    });
+  };
+
+  var particles = Particles.init({
+    selector: '.background',
+    
+  });
